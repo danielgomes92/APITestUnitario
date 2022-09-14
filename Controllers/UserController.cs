de3c9475f3334d59ff86ddf8c06c353d652f099d
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-14 09:21:55 -0300

    teste de identificação de usuário e idade.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using APITestUnitario.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITestUnitario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(User user)
+        {
+            var usuarioDB = new User();
+
+            if (usuarioDB.Nome == user.Nome && 
+                usuarioDB.Idade == user.Idade
+                )
+                return Ok();
+
+            return BadRequest();
+
+        }
+    }
+}
