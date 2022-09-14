b79a38df098f787e456f41e283dda25955cace62
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2022-09-14 09:11:47 -0300

    Criada a pasta Entities e 2 propriedades

---
+++ Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace APITestUnitario.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            Nome = "Daniel";
+            Idade = 30;
+        }
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
